e7a92b0781a285e0548a4100ed795e417afb709d
Laura Berge <lauraeberge@gmail.com>
2019-09-12 02:32:04 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,16 @@
 module Memorable
-  def reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
   
-  def count
-    self.all.count
+    def count
+      self.all.count
+    end
+  end
+  module InstanceMethods
+    def innitialize
+      
+    end
   end
 end
